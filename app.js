193c62595d562969a52fa6d76782e62e38aa1598
jameem <jameemkurikkal.mp@gmail.com>
2020-03-11 21:31:16 +0530

    fetching user repos

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ app.get("/get-user-profile", async (req, res, next) => {
     const clientSecret = process.env.CLIENT_SECRET
     var searchName = req.query.user
 
+    // Fetch user profile
     let url = `https://api.github.com/users/${searchName}?client_id=${clientId}&client_secret=${clientSecret}`
-    // res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
-    console.log("start")
-    const result = await fetch(url, {
+
+    const profile = await fetch(url, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -42,12 +42,35 @@ app.get("/get-user-profile", async (req, res, next) => {
             console.log(err);
         });
 
-        var data = await result.json();
-        console.log(data)
-        if(data){
-            res.json({
-                profile: data
+    var profileData = await profile.json();
+
+
+
+    if (profileData) {
+        // Fetch user repos
+        let reposUrl = `
+        https://api.github.com/users/${searchName}/repos?
+        per_page=10&sort=created:asc&
+        client_id=${clientId}&
+        client_secret=${clientSecret}
+        `
+
+        const profileRepos = await fetch(reposUrl, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
             })
-        }
-        
+            .catch(err => {
+                console.log(err);
+            });
+
+        var repos = await profileRepos.json();
+        console.log(repos)
+        res.json({
+            profile: profileData,
+            repos: repos
+        })
+    }
+
 });
