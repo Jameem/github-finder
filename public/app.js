6e162cc902187727aab36b6895423f504271b5f4
jameem <jameemkurikkal.mp@gmail.com>
2020-03-11 08:18:59 +0530

    setting up timer for keyup

--- public/app.js
+++ public/app.js
@@ -1,16 +1,24 @@
-const github = new Github
-
 //Search input
 const searchUser = document.getElementById('searchUser')
+var timer;
+var x;
 
-searchUser.addEventListener('keyup', (e)=>{
+searchUser.addEventListener('keyup', (e) => {
     const text = e.target.value
-    
-    if(text !==''){
-        //Make http call
-        github.getUser(text)
-        .then(data => {
-            console.log(data)
-        })
-    }
+    if (x) {
+        x.abort()
+    } 
+
+    clearTimeout(timer);
+
+    timer = setTimeout(async function () {
+
+        const response = await fetch(
+            `http://localhost:3000/get-user-profile?user=${text}`
+        )
+
+        var data = await response.json();
+        console.log(data)
+
+    }, 500);
 })
