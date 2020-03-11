0505e1b8406ae5f2908d282a9781fdbb20fcb5c5
jameem <jameemkurikkal.mp@gmail.com>
2020-03-11 07:50:10 +0530

    fetch user profile details

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
+const fetch = require('node-fetch')
 // const {
 //     generateMessage,
 //     generateLocationMessage
@@ -22,3 +23,30 @@ app.use(express.static(publicDirectoryPath))
 app.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
+
+app.get("/url", async (req, res, next) => {
+    const clientId = process.env.CLIENT_ID
+    const clientSecret = process.env.CLIENT_SECRET
+
+    let url = `https://api.github.com/users/Jameem?client_id=${clientId}&client_secret=${clientSecret}`
+    // res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+    console.log("start")
+    const result = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+        var data = await result.json();
+
+        if(data){
+            res.json({
+                profile: data
+            })
+        }
+        
+});
